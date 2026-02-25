60161bd8abe633064c5bb8988dacbcbc94deb4d2
kendall <yung_king2020@icloud.com>
2026-02-24 18:19:22 -0600

    Initial Commit

---
+++ trueworld2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trueworld2.0
+//
+//  Created by Kendall Gipson on 2/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trueworld2.0/trueworld2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  trueworld2_0App.swift
+//  trueworld2.0
+//
+//  Created by Kendall Gipson on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct trueworld2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
